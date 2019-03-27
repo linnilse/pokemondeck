717311c14923d0c8379993a841fe817a5ace0f1f
Linda Nilsson <linda@linnil.se>
2019-03-27 20:16:02 +0100

    Updates

---
+++ precache-manifest.881275e0b2ee31ecf6c2fa3c3e416e66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d95e2698130e183bacd5",
+    "url": "/linnilse/pokemondeck/static/js/runtime~main.f5b94d76.js"
+  },
+  {
+    "revision": "2292d0d5ec073aa5161c",
+    "url": "/linnilse/pokemondeck/static/js/main.65615af8.chunk.js"
+  },
+  {
+    "revision": "9bbf418937168e28e1f4",
+    "url": "/linnilse/pokemondeck/static/js/2.a9e2c106.chunk.js"
+  },
+  {
+    "revision": "2292d0d5ec073aa5161c",
+    "url": "/linnilse/pokemondeck/static/css/main.91cf4cc5.chunk.css"
+  },
+  {
+    "revision": "d3b48d670878088c67b86e955366c8d1",
+    "url": "/linnilse/pokemondeck/index.html"
+  }
+];
